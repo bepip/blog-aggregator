f2f5a14bba686f3141f633060a7f352872083455
bepip <berpi073@hotmail.fr>
2025-12-01 16:58:58 +0100

    Chapter 2

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,10 +1,40 @@
-import { setUser } from "src/config";
+import { readConfig, setUser } from "src/config";
+import { createUser, getUserByName, getUsers } from "src/lib/db/queries/users";
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
+export async function handlerLogin(cmdName: string, ...args: string[]) {
 	if (args.length !== 1) {
 		throw new Error(`usage: ${cmdName} <name>`);
 	}
-	const username = args[0];
-	setUser(username);
-	console.log("User switched successfully!")
+
+	const userName = args[0];
+	const existingUser = await getUserByName(userName);
+	if (!existingUser) {
+		throw new Error(`User ${userName} not found`);
+	}
+
+	setUser(existingUser.name);
+	console.log("User switched successfully!");
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+	if (args.length != 1) {
+		throw new Error(`usage: ${cmdName} <name>`);
+	}
+
+	const userName = args[0];
+	const user = await createUser(userName);
+	if (!user) {
+		throw new Error(`User ${userName} not found`);
+	}
+
+	setUser(user.name);
+	console.log("User created successfully!");
+}
+
+export async function handlerUsers(cmdName: string, ...args: string[]) {
+	const users = await getUsers();
+	const currentUser = readConfig().currentUserName;
+	users.forEach( (user) => {
+		console.log(`\t* ${user.name}`, user.name === currentUser ? "(current)": "");
+	});
 }
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,35 @@
 import { CommandsRegistry, registerCommand, runCommand } from "./commands/commands";
-import { handlerLogin } from "./commands/users";
+import { handlerReset } from "./commands/reset";
+import { handlerLogin, handlerRegister, handlerUsers } from "./commands/users";
 
-function main() {
-  const args = process.argv.slice(2);
+async function main() {
+	const args = process.argv.slice(2);
 
-  if (args.length < 1) {
-    console.log("usage: cli <command> [args...]");
-    process.exit(1);
-  }
+	if (args.length < 1) {
+		console.log("usage: cli <command> [args...]");
+		process.exit(1);
+	}
 
-  const cmdName = args[0];
-  const cmdArgs = args.slice(1);
-  const commandsRegistry: CommandsRegistry = {};
+	const cmdName = args[0];
+	const cmdArgs = args.slice(1);
+	const commandsRegistry: CommandsRegistry = {};
 
-  registerCommand(commandsRegistry, "login", handlerLogin);
+	registerCommand(commandsRegistry, "login", handlerLogin);
+	registerCommand(commandsRegistry, "register", handlerRegister);
+	registerCommand(commandsRegistry, "reset", handlerReset);
+	registerCommand(commandsRegistry, "users", handlerUsers);
 
-  try {
-    runCommand(commandsRegistry, cmdName, ...cmdArgs);
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error(`Error running command ${cmdName}: ${err.message}`);
-    } else {
-      console.error(`Error running command ${cmdName}: ${err}`);
-    }
-    process.exit(1);
-  }
+	try {
+		await runCommand(commandsRegistry, cmdName, ...cmdArgs);
+	} catch (err) {
+		if (err instanceof Error) {
+			console.error(`Error running command ${cmdName}: ${err.message}`);
+		} else {
+			console.error(`Error running command ${cmdName}: ${err}`);
+		}
+		process.exit(1);
+	}
+	process.exit(0);
 }
 
-main();
+await main();
